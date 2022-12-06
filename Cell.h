cfc0891728477f1000b385e29fe085061a5d6b0a
diljara <91018552+diljara@users.noreply.github.com>
2022-12-06 19:42:27 +0300

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -8,7 +8,7 @@ public:
 	sf::RenderWindow* get_window();
 	void render();
 	sf::Vector2f get_coord();
-	std::optional<Entity*> entity_pointer; /*Pointer to entity in this cell*/
+	//std::optional<Entity*> entity_pointer; /*Pointer to entity in this cell*/
 
 private:
 	sf::Vector2f coord;/*координаты*/
